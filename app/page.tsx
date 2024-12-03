91cd6dd491a2bd58cfe6ac607ffdffb612b1a010
Kenny Jinhiro <kenny.jinhiro@unitcode.org>
2024-12-03 08:14:51 +0700

    feat: onboarding

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+// Next Components
+import Image from "next/image";
+
+// Images Path
+import onboardingImagePath from "./images/onboarding.svg";
+
+// Fonts
+import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["600"] });
+
+export default function Home() {
+  return (
+    <>
+      <Onboarding />
+    </>
+  );
+}
+
+function Onboarding() {
+  return (
+    <div className="h-screen w-screen grid grid-cols-1 place-items-center gap-0.5">
+      <Image
+        src={onboardingImagePath}
+        width={352}
+        height={155}
+        alt="Unit's Logo"
+        priority={true}
+      />
+      <h1 className={`${poppins.className} text-3xl`}>
+        Welcome to Unit Network
+      </h1>
+
+      <p className={`${inter.className} m-8 text-center`}>
+        Secure your username, deposit crypto, swap assets, and earn
+        rewards—start now!
+      </p>
+      <button
+        className={`bg-mainPurple ${poppins.className} text-sm px-6 py-3 text-white text-center w-96 h-12 rounded-md transition duration-200 hover:bg-purple-700`}
+      >
+        Get Started
+      </button>
+    </div>
+  );
+}
